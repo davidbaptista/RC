7b008d2333b4140bf0930d35ddf2ec5206cce05f
daviderino <david.baptista00@hotmail.com>
2020-09-29 18:34:09 +0100

    organised code

--- src/pd.c
+++ src/pd.c
@@ -43,26 +43,27 @@ static void parseArgs (long argc, char* const argv[]){
 
 int main(int argc, char *argv[]) {
     int fd, errcode;
-    ssize_t n;
-    socklen_t addrlen;
-    struct addrinfo hints,*res;
-    struct sockaddr_in addr;
+    char line[128];
     char buffer[128];
     char host[NI_MAXHOST], service[NI_MAXSERV];
-    char line[128];
+    struct sockaddr_in addr;
+    struct addrinfo hints, *res;
+    ssize_t n;
+    socklen_t addrlen;
+
 
     parseArgs(argc, argv);
 
-    fd = socket(AF_INET,SOCK_DGRAM,0); //UDP socket
+    fd = socket(AF_INET,SOCK_DGRAM,0); // UDP socket
     if(fd == -1) {
         exit(1);
     }
 
     memset(&hints,0,sizeof hints);
-    hints.ai_family=AF_INET; //IPv4
-    hints.ai_socktype=SOCK_DGRAM; //UDP
+    hints.ai_family=AF_INET; // IPv4
+    hints.ai_socktype=SOCK_DGRAM; // UDP
 
-    if(getaddrinfo(serverIP, serverPort, &hints,&res) != 0) {
+    if(getaddrinfo(serverIP, serverPort, &hints, &res) != 0) {
         exit(1);
     }
 
@@ -76,25 +77,27 @@ int main(int argc, char *argv[]) {
         if(strcmp(command, "exit") == 0) {
             break;
         }
-        else if (strcmp(command, "reg") == 0 && c == 3) {
-
+        else if (strcmp(command, "reg") == 0 && c == 3 && strlen(arg1) == 5 && strlen(arg2) == 8) {
+			puts("uhm");
         }
     }
 
 
-    n = sendto(fd,"Hello!\n", 7, 0, res->ai_addr, res->ai_addrlen);
-    if(n==-1) exit(1);
-
-    addrlen=sizeof(addr);
-    n=recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
-
-    if(n==-1) {
+    n = sendto(fd, "Hello!\n", 7, 0, res->ai_addr, res->ai_addrlen);
+    if(n == -1) {
 		exit(1);
 	}
 
-    write(1,"echo: ",6); 
-	write(1,buffer,n);
-    write(1,buffer,n);
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
+
+    if(n == -1) {
+		exit(1);
+	}
+
+    write(1, "echo: ", 6); 
+	write(1, buffer, n);
+    write(1, buffer, n);
 
     freeaddrinfo(res);
 
